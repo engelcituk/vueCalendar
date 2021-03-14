56f67720ad666edfbcbdf161b3388d2e84b8dc46
cituk <programaciondyn3.mx@sandos.com>
2021-03-14 16:38:22 -0500

    añadiendo modulos

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import vueCustomElement from 'vue-custom-element'
 // (optional) 'Custom elements polyfill'
 import 'document-register-element/build/document-register-element'
 Vue.use(vueCustomElement)
-
 //plugin
-require('./plugins')
-
+require('./plugins') //inclue antDesign para el calendar
 
 Vue.config.productionTip = false
 
 App.store = store
 App.router = router
 Vue.customElement('calendar-widget', App)
+
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
+
 import Vue from 'vue'
 import Vuex from 'vuex'
+import init from '../modules/init'
 
+import VuexPersistence from 'vuex-persist'
 Vue.use(Vuex)
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  modules: []
+
+})
 export default new Vuex.Store({
   state: {
+    loading: false //lo defino aqui porque lo podría ocupar de manera global
   },
-  mutations: {
+  //para modificar el loading
+  mutations:{
+    setLoading(state,bool) {
+      state.loading = bool
+    }
   },
-  actions: {
+  modules:{
+    init
   },
-  modules: {
-  }
+  plugins:[vuexLocal.plugin]
 })
