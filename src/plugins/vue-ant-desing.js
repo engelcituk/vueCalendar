142fafd60a28843c312e42da17bc6349f0304c83
eCituk <ecituk@thefives.com.mx>
2021-03-16 11:53:01 -0500

    Traducir nuestro calendario al Español

--- src/plugins/vue-ant-desing.js
+++ src/plugins/vue-ant-desing.js
@@ -4,6 +4,9 @@ import Vue from 'vue';
 import 'ant-design-vue/dist/antd.css'
 import Calendar from 'ant-design-vue/lib/calendar'
 import Badge from 'ant-design-vue/lib/badge'
+import ConfigProvider from 'ant-design-vue/lib/config-provider'
+
 
 Vue.component('antCalendar', Calendar)
 Vue.component('antBadge', Badge)
+Vue.component('antConfigProvider', ConfigProvider)
