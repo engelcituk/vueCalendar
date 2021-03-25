133036f66f1cd1d221395464e1a54e7055c893d4
eCituk <ecituk@thefives.com.mx>
2021-03-25 18:56:02 -0500

    rango de años

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -47,3 +47,11 @@ export {
     countEventsInLocationByDay
 }
 
+/**
+ const years = (back) => {
+  const year = new Date().getFullYear();
+  return Array.from({length: back}, (v, i) => year - back + i + 1);
+}
+
+console.log(years(3));
+ */
